367ffda31b1ec724dc8781f9eae57866b5b340ef
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-03-31 21:12:47 +0200

    corrected issue with mainnet not working

--- src/wallet/bing_wallet.cpp
+++ src/wallet/bing_wallet.cpp
@@ -1,5 +1,5 @@
-#include "bing_wallet.hpp"
 #include "src/common/bing_common.hpp"
+#include "bing_wallet.hpp"
 
 using namespace std;
 using namespace bc;
@@ -19,10 +19,13 @@ void BingWallet::derive_addresses(
   long_hash seed = electrum::decode_mnemonic(mnemonic);
   data_chunk seedAsChunk(seed.begin(), seed.end());
 
-  uint64_t testnet_mainnet{hd_private::mainnet};
+  uint64_t testnet_or_mainnet{0};
   if (testnet)
-    testnet_mainnet = hd_private::testnet;
-  const hd_private m(seedAsChunk, testnet_mainnet);
+    testnet_or_mainnet = hd_private::testnet;
+  else
+    testnet_or_mainnet = hd_private::mainnet;
+
+  const hd_private m(seedAsChunk, testnet_or_mainnet);
   const hd_public m_pub = m;
 
   auto m0_pub = m.derive_public(0);
@@ -30,12 +33,18 @@ void BingWallet::derive_addresses(
 
   hd_private m0 = m.derive_private(0);
 
+  uint8_t payment_address_version{0};
+  if (testnet)
+      payment_address_version = payment_address::testnet_p2kh;
+  else
+      payment_address_version = payment_address::mainnet_p2kh;
+
   for (int i = 0; i < count; ++i) {
     hd_private hdPrivate = m0.derive_private(i);
     const payment_address address(
-        {hdPrivate.secret(), payment_address::testnet_p2kh});
+        {hdPrivate.secret(), payment_address_version});
     addresses.push_back(address.encoded());
     addresses_to_ec_private[address.encoded()] =
-        ec_private(hdPrivate.secret(), payment_address::testnet_p2kh);
+        ec_private(hdPrivate.secret(), payment_address_version);
   }
 }
--- test/wallet/bing_wallet_test.cpp
+++ test/wallet/bing_wallet_test.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-BOOST_AUTO_TEST_CASE(address_derivation_test) {
+BOOST_AUTO_TEST_CASE(address_derivation_testnet_test) {
   vector<string> addresses;
   map<string, ec_private> addresses_to_ec_private;
   BingWallet::derive_addresses(true,
@@ -15,4 +15,14 @@ BOOST_AUTO_TEST_CASE(address_derivation_test) {
                                "penalty moral unit skate few quick",
                                5, addresses, addresses_to_ec_private);
   BOOST_TEST(addresses.at(4) == "mnasBq7AAYddaYZptduGNQkLdXQjb7PRKt");
+}
+
+BOOST_AUTO_TEST_CASE(address_derivation_mainnet_test) {
+  vector<string> addresses;
+  map<string, ec_private> addresses_to_ec_private;
+  BingWallet::derive_addresses(false,
+                               "effort canal zoo clown shoulder genuine "
+                               "penalty moral unit skate few quick",
+                               5, addresses, addresses_to_ec_private);
+  BOOST_TEST(addresses.at(4) == "184utn2BMXCNoS6DB4vtYVY1mXp2ZiNETW");
 }
