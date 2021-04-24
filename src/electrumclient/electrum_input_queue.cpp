87ae0da74de6df93e600a14ad202e68efbda26dc
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-04-24 20:00:31 +0200

    electrm response blocking queue critical improv

--- src/electrumclient/electrum_input_queue.cpp
+++ src/electrumclient/electrum_input_queue.cpp
@@ -1,5 +1,9 @@
 #include "electrum_input_queue.hpp"
+#include <chrono>
+#include <thread>
+#include <iostream>
 
+using namespace std::chrono_literals;
 using json = nlohmann::json;
 using namespace std;
 
@@ -14,13 +18,22 @@ void ElectrumInputQueue::push(const ElectrumMessage& message) {
 
 ElectrumMessage ElectrumInputQueue::pop_reply(int id) {
     std::unique_lock<std::mutex> lock(mutex_);
-    condition_.wait(lock, [=] {
-        return contains_msg_with_id(id);
-    });
-    auto iter = find_msg_with_id(id);
-    ElectrumMessage m(std::move(*iter));
-    queue_.erase(iter);
-    return m;
+    for(int i = 1; i <= 10; ++i) {
+        if (condition_.wait_for(lock, i*100ms, [=] {
+            return contains_msg_with_id(id);
+            }))
+        {
+            auto iter = find_msg_with_id(id);
+            ElectrumMessage m(std::move(*iter));
+            queue_.erase(iter);
+            //cout << "gotten response for corr id " << id << " in try no " << i << "\n";
+            return m;
+        } else {
+            continue;
+        }
+    }
+    cout << "electrum response missing for correlation id=" << id << "\n";
+    // TODO don't know at the moment what is best to do in such case
 }
 
 bool ElectrumInputQueue::contains_msg_with_id(int id) {
